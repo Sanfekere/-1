a0349e81cb77d5ec52b0c0b85111f096641e76e9
Sanfekere <50259791+Sanfekere@users.noreply.github.com>
2023-01-15 14:34:16 +0200

    Update 1221.py

--- 1221.py
+++ 1221.py
@@ -1,5 +1,10 @@
 n = int(input("Ievadiet sk n: "))
-s = 1
-while n >= 1:
-    s *= n
-    n -= 1
+for i in range(1, n + 1, 6):
+    print(i)
+
+i = 1
+while i < n:
+  print(i)
+  if i == n:
+    break
+  i += 6
