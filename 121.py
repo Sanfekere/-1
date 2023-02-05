5194356a53f1a7f43f8faa01fa9581213eed5ee0
Sanfekere <50259791+Sanfekere@users.noreply.github.com>
2023-02-05 21:49:08 +0200

    Update 121.py

--- 121.py
+++ 121.py
@@ -1,10 +1,10 @@
-judz = 1.609
-i = 5
-x2 = 7.5
-y = float(input("Ievadiet SOLI "))
-while i <= x2:
-    print(round(i, 2), "jūdzes", "|", round(i * judz, 2), "km")
-    i += y 
+# judz = 1.609
+# i = 5
+# x2 = 7.5
+# y = float(input("Ievadiet SOLI "))
+# while i <= x2:
+#     print(round(i, 2), "jūdzes", "|", round(i * judz, 2), "km")
+#     i += y
 
 # y1 = float(input("Ievadiet % cik skriesi nākam dien "))
 # x1 = float(input("Noskrieto km 1 diena "))
@@ -14,7 +14,7 @@ while i <= x2:
 # diena = 0
 
 # while i <= x2:
-#     i += x1*y 
+#     i += x1*y
 #     print(i)
 #     diena += 1
 #     if i == x2:
@@ -22,3 +22,6 @@ while i <= x2:
 
 # print(diena,"dienā" + " tiks noskrieti", x2, "km")
 # print(diena,"dienās" + " tiks noskrieti", x2, "km")
+
+n = int(input("Ievadiet sk n: "))
+
