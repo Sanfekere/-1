9ab759d82c74e3375310ff06be7abff850f3ad55
Sanfekere <linards2006@gmail.com>
2023-02-15 13:29:17 +0200

    Create t.py

---
+++ t.py
@@ -0,0 +1,9 @@
+# Uzdevums par gaisa temperatūru apstrādi (ar list/sarakstu pielietojumu)
+
+# 1) izveidot sarakstu (masīva elementa indekss sakrīt ar kalendāra datumu), kurā jāsaglabā janvāra mēneša prognozētās temperatūras (var izmantot datus, kurus var sameklēt internetā), 
+# 2) vēlams saglabāt gan dienas, gan nakts temperatūru sarakstus (katru savā sarakstā) un rēķināt dienu vidējās temperatūras, kuras izmantotu tālāk uzdevumu risināšanā, 
+# 3) aprēķināt mēneša vidējo temperatūru,
+# 4) siltāko dienu, 
+# 5) aukstāko dienu,
+# 6) izvadīt tikai katras nedēļas nogales dienu temperatūras (sestdienas, svētdienas) (uzdevuma risinājumā ir jāizmanto cikls),
+# 7) noskaidrot siltāko nedēļas nogali un vēsāko, t.i. jāsalīdzina nedēļas nogaļu vidējās temperatūras.
