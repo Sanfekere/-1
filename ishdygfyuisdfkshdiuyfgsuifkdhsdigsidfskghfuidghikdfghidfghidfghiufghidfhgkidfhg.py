96e509a522bb0d4d30adf7d7b30a66043e297c1c
Sanfekere <linards2006@gmail.com>
2023-02-20 13:29:10 +0200

    Update ishdygfyuisdfkshdiuyfgsuifkdhsdigsidfskghfuidghikdfghidfghidfghiufghidfhgkidfhg.py

--- ishdygfyuisdfkshdiuyfgsuifkdhsdigsidfskghfuidghikdfghidfghidfghiufghidfhgkidfhg.py
+++ ishdygfyuisdfkshdiuyfgsuifkdhsdigsidfskghfuidghikdfghidfghidfghiufghidfhgkidfhg.py
@@ -12,12 +12,46 @@
 #    if(sanja[i] < min):    
 #        min = sanja[i]
 #        g = i    
-     
-     
+
+# print("Mazākā vērtība ir", min, "indekss", g+1)    
+
 # 2) Pieprasīt masīvu, kas satur N dažādus veselus skaitļus, atrast lielākās un mazākās vērtības
 # starpību.
+# sanja = []
+# n = int(input("Ievadiet cik skaitļus jūs rakstīsiet? "))
+
+# for i in range(n):
+#     sanja.append(int(input("Ievadiet "+ str(i+1) + ". skaitli: ")))
+
+# min = sanja[0]
+# max = sanja[0]
+
+# for i in range(0, len(sanja)):    
+#    if(sanja[i] < min):    
+#        min = sanja[i]   
+     
+# for i in range(0, len(sanja)):    
+#    if(sanja[i] > max):    
+#        max = sanja[i]    
+
+# print("Lielākā un mazākā vērtības starpība ir ", max-min)
+
 # 3) Sastādīt programmu, kas pieprasa ievadīt 10 skaitļus un paziņo kurš no skaitļiem ir
 # mazākais (ievadīto skaitļu vērtības saglabāt masīvā).
+# sanja = []
+# print("Ievadiet 10 sk ")
+
+# for i in range(10):
+#     sanja.append(int(input("Ievadiet "+ str(i+1) + ". skaitli: ")))
+
+# min = sanja[0]
+
+# for i in range(0, len(sanja)):    
+#    if(sanja[i] < min):    
+#        min = sanja[i] 
+
+# print("Mazākais sk ir:", min)
+
 # 4) Programma pieprasa n veselus skaitļus, saglabā tos masīvā un paziņo to skaitļu skaitu, kas
 # ir mazāki par vidējo aritmētisko.
 # 5) Programma pieprasa n veselus skaitļus, saglabā tos masīvā un paziņo, vai masīvā ir skaitlis,
