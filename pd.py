508d03de453253674e60e2748aa2aae33e8f6d49
Sanfekere <linards2006@gmail.com>
2023-03-06 12:19:54 +0200

    Update pd.py

--- pd.py
+++ pd.py
@@ -6,3 +6,15 @@ for i in range(linards):
     vaivods.append(random.randrange(-150, 150))
 
 print(vaivods)
+
+pozitivi = 0
+nepozitivi = 0
+
+for i in range(linards):
+    if vaivods[i] >= 0:
+        pozitivi += 1
+    elif vaivods[i] >= 0:
+        nepozitivi += 1
+    i += 1
+
+print(pozitivi)
