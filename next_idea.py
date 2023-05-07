02ae3bcc8118c5f46c783c569a16acdb60bb9ec6
Sanfekere <50259791+Sanfekere@users.noreply.github.com>
2023-05-07 19:16:09 +0300

    njaaa

--- next_idea.py
+++ next_idea.py
@@ -12,7 +12,6 @@ print("4. Clicker Simulator")
 
 while True:
     user_choice = input("Lai izvēlētos, ievadiet atbilstošo ciparu: ")
-
     if user_choice not in ["1", "2", "3", "4"]:
         print("Nepareiza ievade. Lūdzu, ievadiet atbilstošo ciparu no 1 līdz 4.")
         continue
