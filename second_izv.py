1312406e721bc725d88f25fc498a36304f1621cb
Sanfekere <50259791+Sanfekere@users.noreply.github.com>
2023-05-14 20:37:55 +0300

    uzlabojumi

---
+++ second_izv.py
@@ -0,0 +1,38 @@
+import tkinter as tk
+import bizness
+
+def atvert_bankas_simulatoru():
+    import Banka
+
+
+def atvert_clicker_simulatoru():
+    bizness.simulator()
+
+def izveleties_speli():
+    user_choice = choice_var.get()
+    if user_choice == "1":
+        print("Jūs izvēlējāties spēli Banka.")
+        atvert_bankas_simulatoru()
+    elif user_choice == "2":
+        print("Jūs izvēlējāties spēli Clicker Simulator(Bizzness).")
+        atvert_clicker_simulatoru()
+
+root = tk.Tk()
+root.title("Spēļu simulatora galvenais izvēlnis")
+
+label = tk.Label(root, text="==== Spēļu simulatora galvenais izvēlnis ====")
+label.pack()
+
+choice_var = tk.StringVar()
+
+banka_btn = tk.Radiobutton(root, text="Banka", variable=choice_var, value="1")
+banka_btn.pack()
+
+
+clicker_btn = tk.Radiobutton(root, text="Clicker Simulator(Bizzness)", variable=choice_var, value="2")
+clicker_btn.pack()
+
+select_btn = tk.Button(root, text="Izvēlēties", command=izveleties_speli)
+select_btn.pack()
+
+root.mainloop()
