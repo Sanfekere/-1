c7fcfe684621597bb47d11011c0a8deb13a3fa1e
Sanfekere <50259791+Sanfekere@users.noreply.github.com>
2023-09-11 14:49:38 +0300

    Create hello_world.py

---
+++ hello_world.py
@@ -0,0 +1,25 @@
+manadisc = {
+    "marka" : "BMW",
+    "modelis" : "M4",
+    "tilpums" : "3.0",
+    "degviela" : "BENZĪNS",
+    "gads" : "2022",
+    "Pirmā reģistrācija" : '2022-02',
+    "Nobraukumsja attālums" : "26 500 km",
+    "Dzinējs" : "2993 cm³, 510 ZS (375kW)",
+    "Degvielas tips" : "Benzīns",
+    "Ķermeņa tips" : "Kupeja (kupeja)",
+    "Durvju skaits" : "2/3",
+    "Piedziņas riteņi" : "Visu disku (4х4)",
+    "Pārnesumkārba" : "Automātisks",
+    "Klimata pārvaldība" : "Klimata kontrole",
+    "Krāsu" : "Zaļš / haki",
+    "Stūres rata stāvoklis" : "Pa kreisi",
+    "Tehniskais inspekcijā pēc" : "2025-07",
+    "Loka diametrs" : "R20",
+    "Sēdvietu skaits" : "4",
+    "CO₂ izmeši, g/km" : "227 g/km"
+    }
+
+
+print(manadisc)
