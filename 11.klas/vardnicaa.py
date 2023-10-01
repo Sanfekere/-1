e8e72e50c0707b801f8c7557d5794da4feafe1ba
Sanfekere <50259791+Sanfekere@users.noreply.github.com>
2023-10-01 20:26:17 +0300

    Update vardnicaa.py

--- 11.klas/vardnicaa.py
+++ 11.klas/vardnicaa.py
@@ -1,84 +1,47 @@
-from selenium import webdriver
-from selenium.webdriver.common.keys import Keys
-from selenium.common.exceptions import NoSuchElementException
-from selenium.common.exceptions import JavascriptException
+# pip install deep-translator
 
-# local variables
-from selenium.webdriver.chrome.options import Options as ChromeOptions
-chrome_op = ChromeOptions()
-chrome_op.add_argument('--headless')
-browser = webdriver.Chrome(executable_path ='chromedriver', options = chrome_op)
+from deep_translator import GoogleTranslator
 
+vardnica = {
+    'suns': {'en': 'dog', 'de': 'Hund'},
+    'kaķis': {'en': 'cat'}
+}
 
-def takeInput():
-	languages = {"English": 'en', "French": 'fr',
-				"Spanish": 'es', "German": 'de', "Italian": 'it'}
+def tulkojums(ievaditais_vards, uz_valodu):
+    if ievaditais_vards in vardnica:
+        if uz_valodu in vardnica[ievaditais_vards]:
+            return vardnica[ievaditais_vards][uz_valodu]
+        else:
+            print(f"Šis vārds ir vārdnīcā, bet netiek atrasts {uz_valodu} tulkojums. (LŪDZU PARLIECINĀTIES PAR VĀRDA PAREIZU TŪLKOJUMU)")
+            tulkots = GoogleTranslator(source='auto', target=f'{uz_valodu}').translate(ievaditais_vards)
+            vardnica[ievaditais_vards][uz_valodu] = tulkots
+            return tulkots
+    else:
+        print("Diemžēl šis vārds nav atrodams vārdnīcā, bet tas tiks iztulkots izmantojot tulkotāju. (LŪDZU PARLIECINĀTIES PAR VĀRDA PAREIZU TŪLKOJUMU)")
+        tulkots = GoogleTranslator(source='auto', target=f'{uz_valodu}').translate(ievaditais_vards)
+        vardnica[ievaditais_vards] = {uz_valodu: tulkots}
+        return tulkots
 
-	print("Select a source and target language (enter codes)")
-	print("Language", " ", "Code")
+while True:
+    print('Ievadiet tulkojamo vārdu latviešu valodā (beigt lai izietu, vardnica lai apskatītu vārdnīcu): ')
+    ievade = input().strip().lower()
 
-	for x in languages:
-		print(x, " ", languages[x])
+    if ievade == 'beigt':
+        break
+    elif ievade == 'vardnica':
+        print('Izvadīt visu vai kādu noteiktu vārdu(suns,kaķis u.c): ')
+        var = input().strip().lower()
+        if var == 'visi':
+            print(vardnica)
+        else:
+            print(vardnica[f'{var}'])
+        continue
 
-	print("\n\nSource: ", end ="")
-	src = input()
-	sflag = 0
+    print('Uz kuru valodu tulkot (en, de): ')
+    valoda = input().strip().lower()
 
-	for x in languages:
-		if(languages[x] == src and not sflag):
-			sflag = 1
-			break
-	if(not sflag):
-		print("Source code not from the list, Exiting....")
-		exit()
+    iztulkots = tulkojums(ievade, valoda)
 
-	print("Target: ", end ="")
-	trg = input()
-	tflag = 0
+    print('Vārda tulkojums ir', iztulkots)
 
-	for x in languages:
-		if(languages[x] == trg and not tflag):
-			tflag = 1
-			break
-
-	if(not tflag):
-		print("Target code not from the list, Exiting....")
-		exit()
-
-	if(src == trg):
-		print("Source and Target cannot be same, Exiting...")
-		exit()
-
-	print("Enter the phrase: ", end ="")
-	phrase = input()
-
-	return src, trg, phrase
-
-def makeCall(url, script, default):
-	response = default
-	try:
-		browser.get(url)
-		while(response == default):
-			response = browser.execute_script(script)
-
-	except JavascriptException:
-		print(JavascriptException.args)
-
-	except NoSuchElementException:
-		print(NoSuchElementException.args)
-
-	if(response != default):
-		return response
-	else:
-		return 'Not Available'
-
-
-def googleTranslate(src, trg, phrase):
-	url = 'https://translate.google.co.in/# view = home&op = translate&sl =' + \
-		src + '&tl =' + trg+'&text ='+phrase
-	script = 'return document.getElementsByClassName("tlid-translation")[0].textContent'
-	return makeCall(url, script, None)
-
-if __name__ == "__main__":
-	src, trg, phrase = takeInput()
-	print("\nResult: ", googleTranslate(src, trg, phrase))
+print('Programma beidza darbu!')
