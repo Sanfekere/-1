327afb42a895930a6a4ccbd01d6e5c202224d417
Sanfekere <50259791+Sanfekere@users.noreply.github.com>
2023-10-30 20:31:18 +0200

    Update darbs.py

--- 11.klas/darbs.py
+++ 11.klas/darbs.py
@@ -1,4 +1,22 @@
+def pasuti_tkreklus(skaits, apdruka, piegade):
 
+    cena_apdruka = {'TEKSTS': 5, 'ZIME': 7, 'FOTO': 20}
 
-def pasutit_tkreklus(sk, apdruka, piegade):
-    
+    apdrukas_cena = cena_apdruka.get(apdruka, 0) * skaits
+
+    summa_pirms_atlaides_un_piegades = apdrukas_cena
+
+    if summa_pirms_atlaides_un_piegades > 100:
+        summa_pirms_atlaides_un_piegades *= 0.95
+
+    if piegade and summa_pirms_atlaides_un_piegades < 50:
+        piegades_maksa = 15
+    else:
+        piegades_maksa = 0
+
+    kopsumma = summa_pirms_atlaides_un_piegades + piegades_maksa
+
+    return kopsumma
+
+cena = pasuti_tkreklus(50, 'ZIME', True)
+print(cena)
