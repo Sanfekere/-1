0b4bdb8a2dd2d1f4304ff9f1c0ab002098a1e06e
Sanfekere <50259791+Sanfekere@users.noreply.github.com>
2024-02-05 14:52:50 +0200

    FGFGJGY

--- 11.klas/html/trijsturis/script.js
+++ 11.klas/html/trijsturis/script.js
@@ -4,8 +4,23 @@ function searchURL() {
     var c = Number(document.getElementById("c").value);
 
     if (a + b > c && a + c > b && b + c > a) {
-        document.getElementById("rezultats").innerHTML = "Trijsturis ir iespējams";
+        document.getElementById("rezultats").innerHTML = "Rezultāts: " + "Trijsturis ir iespējams";
+        rezult()
     } else {
-        document.getElementById("rezultats").innerHTML = "Trijsturis nav iespējams";
+        document.getElementById("rezultats").innerHTML = "Rezultāts: " + "Trijsturis nav iespējams";
+        document.getElementById("laukums").innerHTML ="Laukums: " + "Trijsturis nav laukuma, jo tas ir nēiespējams";
     }
+}
+
+
+function  rezult() {
+    var a = Number(document.getElementById("a").value);
+    var b = Number(document.getElementById("b").value);
+    var c = Number(document.getElementById("c").value);
+
+    let s = (a + b + c) / 2;
+    let area = Math.sqrt(s * ((s - a) * (s - b) * (s - c)));
+
+
+    document.getElementById("laukums").innerHTML ="Laukums: " + area + " cm2";
 }
